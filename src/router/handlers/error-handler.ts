22a2ba79da36f0ab25346f38b605069800df90d3
Kequc <v2xyvxkr.1646bpmi@getinbox.io>
2026-08-25 00:27:05 +0200

    Fix dev behavior

--- src/router/handlers/error-handler.ts
+++ src/router/handlers/error-handler.ts
@@ -1,14 +1,26 @@
 import { createErrorHandler } from '../modules.ts';
 
+interface ErrorResponse {
+    statusCode: number;
+    message: string;
+    cause?: unknown;
+    stack?: string[];
+    info?: Record<string, unknown>;
+}
+
 export const errorHandler = createErrorHandler({
     contentType: '*',
     action(ex, { res }) {
         res.setHeader('Content-Type', 'application/json');
-        const error = {
+        const error: ErrorResponse = {
             statusCode: ex.statusCode,
             message: ex.message,
             cause: ex.cause,
         };
+        if (process.env.NODE_ENV !== 'production') {
+            error.stack = ex.stack?.split(/\r?\n/);
+            error.info = ex.info;
+        }
         return { error };
     },
 });
